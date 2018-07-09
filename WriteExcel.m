09fd0891afa23a999a8e199061a0641df096eb4d
weiliangz <weiliangzhou93@gmail.com>
2018-07-09 11:15:10 +1000

    update with 6 hedging scenarios

--- WriteExcel.m
+++ WriteExcel.m
@@ -1,35 +1,35 @@
-% The whole data is 55*300*4*2=132000 rows and 7 columns
+% The whole data is 55*300*6*2=198000 rows and 7 columns
 % Columns: SimNo,WeekNo,WeeklyRevenue,4WeekRollingWeekNo,MonthlyRevenue,ScenarioNo,SpotNo
 
-Output = zeros(132000,7);
+Output = zeros(198000,7);
 
 % Simulation numbers
-SimNo = zeros(132000,1);
-temp1 = zeros(440,300);
+SimNo = zeros(198000,1);
+temp1 = zeros(660,300);
 for i = 1:300
     temp1(:,i) = i;
 end
 for i = 1:300
-    SimNo(1+440*(i-1):440+440*(i-1)) = temp1(:,i);
+    SimNo(1+660*(i-1):660+660*(i-1)) = temp1(:,i);
 end
 % Week numbers
-WeekNo = zeros(132000,1);
+WeekNo = zeros(198000,1);
 temp2 = zeros(55,1);
-temp = zeros(440,1);
+temp = zeros(660,1);
 for i = 1:55
     temp2(i) = i;
 end
-for i = 1:8
+for i = 1:12
     temp(1+55*(i-1):55+55*(i-1)) = temp2(:);
 end
 for i = 1:300
-    WeekNo(1+440*(i-1):440+440*(i-1)) = temp(:);
+    WeekNo(1+660*(i-1):660+660*(i-1)) = temp(:);
 end
 % Weekly Revenue
-WeekRevenue = zeros(132000,1);
+WeekRevenue = zeros(198000,1);
 count = 0;
 for i = 1:300
-    for j = 1:4
+    for j = 1:6
         for k = 1:2
             WeekRevenue(1+55*count : 55+55*count) = weekly_revenue(:,i,j,k);
             count = count + 1;
@@ -37,10 +37,10 @@ for i = 1:300
     end
 end
 % Monthly Revenue
-MonthRevenue = zeros(132000,1);
+MonthRevenue = zeros(198000,1);
 count = 0;
 for i = 1:300
-    for j = 1:4
+    for j = 1:6
         for k = 1:2
             MonthRevenue(1+55*count : 52+55*count) = monthly_revenue(:,i,j,k);
             MonthRevenue(53+55*count : 55+55*count) = 0;
